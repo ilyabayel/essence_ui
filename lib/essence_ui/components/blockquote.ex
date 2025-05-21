628d0755321c96861bc7b573f5503777ad888432
Ilya Bayaliyev <ilyabayaliyev@gmail.com>
2025-05-21 08:39:44 +0500

    feat: add blockquote component

---
+++ lib/essence_ui/components/blockquote.ex
@@ -0,0 +1,85 @@
+defmodule EssenceUI.Components.Blockquote do
+  @moduledoc """
+  Blockquote component 100% compatible with Radix UI Themes Blockquote API.
+  See: https://www.radix-ui.com/themes/docs/components/blockquote
+  """
+
+  use Phoenix.Component
+
+  alias EssenceUI.Helpers.ExtractProps
+  alias EssenceUI.SharedProps.ColorProps
+  alias EssenceUI.SharedProps.HighContrastProps
+  alias EssenceUI.SharedProps.TruncateProps
+  alias EssenceUI.SharedProps.WeightProps
+
+  require ColorProps
+  require HighContrastProps
+  require TruncateProps
+  require WeightProps
+
+  @doc """
+  Renders a blockquote. All props match Radix UI Blockquote.
+
+  ## Examples
+
+      <.blockquote>
+        <span :slot>Quote content here.</span>
+      </.blockquote>
+
+  ## Props
+
+    * `color` - The accent color. One of: gray, gold, bronze, brown, yellow, amber, orange, tomato, red, ruby, crimson, pink, plum, purple, violet, iris, indigo, blue, cyan, teal, jade, green, grass, lime, mint, sky.
+    * `high_contrast` - Whether to increase color contrast with the background.
+    * `class` - Additional CSS classes to add to the element.
+    * `style` - Additional inline styles.
+    * `rest` - Additional HTML attributes.
+  """
+
+  ColorProps.attrs()
+  HighContrastProps.attrs()
+  WeightProps.attrs()
+  TruncateProps.attrs()
+  attr :size, :string, values: Enum.map(1..9, &Integer.to_string/1), doc: "Blockquote size (1-9). Responsive supported."
+  attr :class, :string, default: nil, doc: "Additional CSS classes."
+  attr :style, :string, default: nil, doc: "Additional inline styles."
+  attr :rest, :any, default: %{}, doc: "Additional HTML attributes."
+  slot :inner_block, required: true
+
+  def blockquote(assigns) do
+    prop_defs =
+      %{
+        size: %{type: :enum, values: Enum.map(1..9, &Integer.to_string/1), class: "rt-r-size", responsive: true}
+      }
+      |> Map.merge(ColorProps.color_prop_def())
+      |> Map.merge(HighContrastProps.prop_defs())
+      |> Map.merge(WeightProps.prop_defs())
+      |> Map.merge(TruncateProps.prop_defs())
+
+    extracted = ExtractProps.call(assigns, prop_defs)
+
+    class =
+      ["rt-Text", "rt-Blockquote", extracted.class]
+      |> Enum.filter(& &1)
+      |> Enum.join(" ")
+
+    style =
+      [extracted.style, assigns[:style]]
+      |> Enum.filter(& &1)
+      |> Enum.join("; ")
+
+    color = assigns[:color] || false
+
+    assigns =
+      assign(assigns,
+        class: class,
+        style: style,
+        color: color
+      )
+
+    ~H"""
+    <blockquote class={@class} style={@style} data-accent-color={@color} {@rest}>
+      {render_slot(@inner_block)}
+    </blockquote>
+    """
+  end
+end
---
+++ storybook/typography/blockquote.story.exs
@@ -0,0 +1,141 @@
+defmodule EssenceUIWeb.Storybook.Typography.Blockquote do
+  @moduledoc false
+  use PhoenixStorybook.Story, :component
+
+  alias EssenceUI.Components.Blockquote
+
+  def function, do: &Blockquote.blockquote/1
+
+  def container,
+    do: {:div, "data-scaling": "100%", "data-radius": "medium", style: "width: 100%;", "data-accent-color": "indigo"}
+
+  def layout, do: :one_column
+
+  def variations do
+    [
+      %Variation{
+        id: :default,
+        description: "Default",
+        attributes: %{},
+        slots: [
+          "Perfect typography is certainly the most elusive of all arts. Sculpture in stone alone comes near it in obstinacy."
+        ]
+      },
+      %Variation{
+        id: :sizes,
+        description: "Size",
+        template: """
+        <div class="rt-Flex rt-r-fd-column rt-r-gap-5">
+          <div style="max-width: 300px;">
+            <.blockquote size="1">
+              Perfect typography is certainly the most elusive of all arts. Sculpture in stone alone comes near it in obstinacy.
+            </.blockquote>
+          </div>
+          <div style="max-width: 350px;">
+            <.blockquote size="2">
+              Perfect typography is certainly the most elusive of all arts. Sculpture in stone alone comes near it in obstinacy.
+            </.blockquote>
+          </div>
+          <div style="max-width: 400px;">
+            <.blockquote size="3">
+              Perfect typography is certainly the most elusive of all arts. Sculpture in stone alone comes near it in obstinacy.
+            </.blockquote>
+          </div>
+          <div style="max-width: 450px;">
+            <.blockquote size="4">
+              Perfect typography is certainly the most elusive of all arts. Sculpture in stone alone comes near it in obstinacy.
+            </.blockquote>
+          </div>
+          <div style="max-width: 500px;">
+            <.blockquote size="5">
+              Perfect typography is certainly the most elusive of all arts. Sculpture in stone alone comes near it in obstinacy.
+            </.blockquote>
+          </div>
+          <div style="max-width: 550px;">
+            <.blockquote size="6">
+              Perfect typography is certainly the most elusive of all arts. Sculpture in stone alone comes near it in obstinacy.
+            </.blockquote>
+          </div>
+          <div style="max-width: 600px;">
+            <.blockquote size="7">
+              Perfect typography is certainly the most elusive of all arts. Sculpture in stone alone comes near it in obstinacy.
+            </.blockquote>
+          </div>
+          <div style="max-width: 650px;">
+            <.blockquote size="8">
+              Perfect typography is certainly the most elusive of all arts. Sculpture in stone alone comes near it in obstinacy.
+            </.blockquote>
+          </div>
+          <div style="max-width: 1000px;">
+            <.blockquote size="9">
+              Perfect typography is certainly the most elusive of all arts. Sculpture in stone alone comes near it in obstinacy.
+            </.blockquote>
+          </div>
+        </div>
+        """
+      },
+      %Variation{
+        id: :weights,
+        description: "Weight",
+        template: """
+        <div class="rt-Flex rt-r-fd-column rt-r-gap-3" style="max-width: 500px;">
+          <.blockquote weight="regular">
+            Perfect typography is certainly the most elusive of all arts. Sculpture in stone alone comes near it in obstinacy.
+          </.blockquote>
+          <.blockquote weight="medium">
+            Perfect typography is certainly the most elusive of all arts. Sculpture in stone alone comes near it in obstinacy.
+          </.blockquote>
+          <.blockquote weight="bold">
+            Perfect typography is certainly the most elusive of all arts. Sculpture in stone alone comes near it in obstinacy.
+          </.blockquote>
+        </div>
+        """
+      },
+      %Variation{
+        id: :colors,
+        description: "Color",
+        template: """
+        <div class="rt-Flex rt-r-fd-column rt-r-gap-3" style="max-width: 500px;">
+          <.blockquote color="indigo">
+            Perfect typography is certainly the most elusive of all arts. Sculpture in stone alone comes near it in obstinacy.
+          </.blockquote>
+          <.blockquote color="cyan">
+            Perfect typography is certainly the most elusive of all arts. Sculpture in stone alone comes near it in obstinacy.
+          </.blockquote>
+          <.blockquote color="orange">
+            Perfect typography is certainly the most elusive of all arts. Sculpture in stone alone comes near it in obstinacy.
+          </.blockquote>
+          <.blockquote color="crimson">
+            Perfect typography is certainly the most elusive of all arts. Sculpture in stone alone comes near it in obstinacy.
+          </.blockquote>
+        </div>
+        """
+      },
+      %Variation{
+        id: :high_contrast,
+        description: "High-contrast",
+        template: """
+        <div class="rt-Flex rt-r-fd-column rt-r-gap-3" style="max-width: 500px;">
+          <.blockquote color="gray">
+            Perfect typography is certainly the most elusive of all arts. Sculpture in stone alone comes near it in obstinacy.
+          </.blockquote>
+          <.blockquote color="gray" high_contrast={true}>
+            Perfect typography is certainly the most elusive of all arts. Sculpture in stone alone comes near it in obstinacy.
+          </.blockquote>
+        </div>
+        """
+      },
+      %Variation{
+        id: :truncate,
+        description: "Truncate",
+        template: """
+        <div class="rt-Flex" style="max-width: 500px;">
+          <.blockquote truncate>
+            Perfect typography is certainly the most elusive of all arts. Sculpture in stone alone comes near it in obstinacy.
+          </.blockquote>
+        </div>
+        """
+      }
+    ]
+  end
+end
