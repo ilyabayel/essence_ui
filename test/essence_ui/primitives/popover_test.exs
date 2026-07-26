99394f970fccda04be39c706c6fcc3bfa7d30d64
Ilya Bayaliyev <ilyabayaliyev@gmail.com>
2026-07-27 00:35:59 +0500

    minor chages

--- test/essence_ui/primitives/popover_test.exs
+++ test/essence_ui/primitives/popover_test.exs
@@ -41,6 +41,29 @@ defmodule EssenceUI.Primitives.PopoverTest do
     assert html =~ "display: none"
   end
 
+  test "trigger and close support as=div for nested buttons" do
+    html =
+      render_component(
+        fn assigns ->
+          ~H"""
+          <Popover.root id="as-div">
+            <Popover.trigger as="div"><button type="button">Open</button></Popover.trigger>
+            <Popover.portal id="as-div-portal">
+              <Popover.content id="as-div-content">
+                <Popover.close as="div"><button type="button">Done</button></Popover.close>
+              </Popover.content>
+            </Popover.portal>
+          </Popover.root>
+          """
+        end,
+        %{}
+      )
+
+    assert html =~ ~r/<div[^>]*data-essence-popover-trigger/
+    assert html =~ ~r/<div[^>]*data-essence-popover-close/
+    assert html =~ ~r/data-essence-popover-trigger[^>]*>[\s\S]*<button[^>]*>Open/
+  end
+
   test "portal thin-wraps Phoenix portal" do
     html =
       render_component(
