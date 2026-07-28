340d435611349b00e5508e9f87c324d34cb2d299
Ilya Bayaliyev <ilyabayaliyev@gmail.com>
2026-07-28 16:52:51 +0500

    bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule EssenceUI.MixProject do
   def project do
     [
       app: :essence_ui,
-      version: "0.1.0",
+      version: "0.2.0",
       elixir: "~> 1.15",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
